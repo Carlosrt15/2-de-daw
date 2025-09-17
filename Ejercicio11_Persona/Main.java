62ba945452d15a57c828c97a5c41b05643d98609
Carlos <carlosrt123450@gmail.com>
2025-09-17 12:19:49 +0200

    Ejercicio 11_Persona

---
+++ Ejercicio11_Persona/Main.java
@@ -0,0 +1,17 @@
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        System.out.println("Introduce un nombre");
+        
+
+
+
+    }
+}
