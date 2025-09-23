73ae12e7a70520db34d9a8228095590880aa2da6
Carlos <carlosrt123450@gmail.com>
2025-09-23 12:28:53 +0200

    Creando el tipoModelo

---
+++ java/Ejercicio3DB/model/TipoDTO.java
@@ -0,0 +1,38 @@
+package model;
+
+public class TipoDTO {
+    private int id;
+    private String descripcion;
+
+    // Constructor
+    public TipoDTO(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "TipoDTO{" +
+                "id=" + id +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
---
+++ java/Ejercicio3DB/model/TipoModelo.java
@@ -0,0 +1,13 @@
+
+package model;
+
+public class TipoModelo {
+
+    public Integer insertarTipo(int id, String descripcion) throws SQLException, ClassNotFoundException
+    
+           
+    {
+             String sql = "INSERT INTO tipo (idtipo, descripcion) VALUES (?, ?)";
+    }
+    
+}
