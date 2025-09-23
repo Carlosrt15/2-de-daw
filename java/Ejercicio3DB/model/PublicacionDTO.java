ea4423fd85bec066febe17cba61f3d2535d0069d
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-09-23 20:08:58 +0200

    Insertar en la tabla publicacion

--- java/Ejercicio3DB/model/PublicacionDTO.java
+++ java/Ejercicio3DB/model/PublicacionDTO.java
@@ -8,6 +8,82 @@ public class PublicacionDTO {
     private double precio;
     private int stock;
     private String idtipo;
+    
+	public PublicacionDTO(String idpublicacion, String titulo, String autor, int nroedicion, double precio, int stock,
+			String idtipo) {
+		super();
+		this.idpublicacion = idpublicacion;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.nroedicion = nroedicion;
+		this.precio = precio;
+		this.stock = stock;
+		this.idtipo = idtipo;
+	}
+
+	public String getIdpublicacion() {
+		return idpublicacion;
+	}
+
+	public void setIdpublicacion(String idpublicacion) {
+		this.idpublicacion = idpublicacion;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getNroedicion() {
+		return nroedicion;
+	}
+
+	public void setNroedicion(int nroedicion) {
+		this.nroedicion = nroedicion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getIdtipo() {
+		return idtipo;
+	}
+
+	public void setIdtipo(String idtipo) {
+		this.idtipo = idtipo;
+	}
+
+	@Override
+	public String toString() {
+		return "PublicacionDTO [idpublicacion=" + idpublicacion + ", titulo=" + titulo + ", autor=" + autor
+				+ ", nroedicion=" + nroedicion + ", precio=" + precio + ", stock=" + stock + ", idtipo=" + idtipo + "]";
+	}
+	
+	
 
 
 }
--- java/Ejercicio3DB/model/PublicacionModelo.java
+++ java/Ejercicio3DB/model/PublicacionModelo.java
@@ -1,5 +1,32 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PublicacionModelo {
+
+        public Integer insertarPublicacion(String idpublicacion, String titulo, String autor,int nroedicion, double precio, int stock,String idtipo) throws SQLException, ClassNotFoundException {
+
+            String sql = "INSERT INTO publicacion (idpublicacion,titulo,autor,nroedicion,precio,stock,idtipo) VALUES (?,?,?,?,?,?,?)";
+
+            try (Connection conn = ConexionDB.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+                    ps.setString(1, idpublicacion);
+                    ps.setString(2, titulo);
+                    ps.setString(3, autor);
+                    ps.setInt(4, nroedicion);
+                    ps.setDouble(5, precio);
+                    ps.setInt(6, stock );
+                    ps.setString(7, idtipo);
+
+                    return ps.executeUpdate();
+                    
+
+                }
+
+    }
+
 
 }
