822a7acbfdbdac9f21f387be3706d423522ebec5
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-09-23 21:21:09 +0200

    Creando el crud de publicacion insertar

--- java/Ejercicio3DB/controller/PublicacionController.java
+++ java/Ejercicio3DB/controller/PublicacionController.java
@@ -1,5 +1,20 @@
 package controller;
 
+import java.sql.SQLException;
+
+import model.PublicacionModelo;
+
 public class PublicacionController {
+
+    public Integer insertarPublicacion(String idpublicacion, String titulo, String autor,int nroedicion, double precio, int stock,String idtipo) throws SQLException, ClassNotFoundException {
+
+            PublicacionModelo pm = new PublicacionModelo();
+                return pm.insertarPublicacion(idpublicacion, titulo, autor, nroedicion, precio, stock, idtipo);
+
+
+
+
+}
+
 
 }
--- java/Ejercicio3DB/view/VistaPublicacion.java
+++ java/Ejercicio3DB/view/VistaPublicacion.java
@@ -1,5 +1,16 @@
 package view;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class VistaPublicacion {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void menuInsertarPublicacion() throws  ClassNotFoundException, SQLException,
+        NumberFormatException, IOException {
+
+            
 
 }
