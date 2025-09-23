9899927f2d2765fcdda4576ec5c0cf75631cb46b
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-09-23 19:19:00 +0200

    Empezamos el crud de la tabla (publicacion)

---
+++ java/Ejercicio3DB/model/PublicacionDTO.java
@@ -0,0 +1,13 @@
+package model;
+
+public class PublicacionDTO {
+    private String idpublicacion;
+    private String titulo;
+    private String autor;
+    private int nroedicion;
+    private double precio;
+    private int stock;
+    private String idtipo;
+
+
+}
