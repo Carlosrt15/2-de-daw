e84e0de009368530347f88575e07ffd9c363b56f
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-09-24 10:18:22 +0200

    Ya se puede insertar en la tabla publicacion

--- java/Ejercicio3DB/Main.java
+++ java/Ejercicio3DB/Main.java
@@ -1,19 +1,25 @@
 import java.io.IOException;
 import java.sql.SQLException;
 
+import view.VistaPublicacion;
 import view.VistaTipo;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         System.out.println("Hola mundo");
 
+      VistaPublicacion vp = new VistaPublicacion();
         VistaTipo vt = new VistaTipo();
 
       //  vt.menuInsertarTipo();
 
       //  vt.menuListarTipos();
 
-        vt.menuActualizarDescripcion();
+       // vt.menuActualizarDescripcion();
+       vp.menuInsertarPublicacion();
+
+
+
 
     }
 }
--- java/Ejercicio3DB/view/VistaPublicacion.java
+++ java/Ejercicio3DB/view/VistaPublicacion.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import controller.PublicacionController;
+
 public class VistaPublicacion {
 
     Scanner sc = new Scanner(System.in);
@@ -11,6 +13,27 @@ public class VistaPublicacion {
     public void menuInsertarPublicacion() throws  ClassNotFoundException, SQLException,
         NumberFormatException, IOException {
 
-            
+            System.out.println("Inserta la idPublicacion");
+            String idpublicacion = sc.nextLine();
+            System.out.println("Inserta el titulo");
+            String titulo = sc.nextLine();
+            System.out.println("Inserta el autor");
+            String autor = sc.nextLine();
+            System.out.println("Inserta el numero de edicion");
+            int nroedicion = sc.nextInt();
+            System.out.println("Inserta el precio ");
+            double precio = sc.nextDouble();
+            System.out.println("Inserta el numero de stock");
+            int stock = sc.nextInt();
+           
+            String idtipo = "COM";
+
+            PublicacionController controller = new PublicacionController();
+            Integer filas = controller.insertarPublicacion(idpublicacion, titulo, autor, nroedicion, precio, stock, idtipo);
+
+            System.out.println("Filas insertadas: " +filas);
+        }
+
+
 
 }
