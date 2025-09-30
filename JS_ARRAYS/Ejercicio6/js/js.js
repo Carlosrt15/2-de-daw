24b205368066c5b41bfe1f79cc7642828fbf527f
Carlos <carlosrt123450@gmail.com>
2025-09-30 14:10:12 +0200

    Terminar numeros de telefono

--- JS_ARRAYS/Ejercicio6/js/js.js
+++ JS_ARRAYS/Ejercicio6/js/js.js
@@ -21,6 +21,55 @@ function ejercicio6() {
     // Comprobar aprobados
 
      let comprobarAprobado = notas.every(nota => nota[0] >= 5);
-    console.log(comprobarAprobado);
+
+        if(comprobarAprobado != true) {
+            alert("No todos aprobaron");
+        } else {
+            alert("Todos aprobaron");
+        }
+
+    /*
+   A partir de un array que contenga 12 palabras, obtén otro en el que solo se
+    ncluyan las que tengan menos de 6 caracteres.
+  */
+
+    let palabras = [
+  "sol",
+  "luna",
+  "mar",
+  "cielo",
+  "montaña",
+  "río",
+  "bosque",
+  "flor",
+  "estrella",
+  "nube",
+  "arena",
+  "viento"
+];
+
+let menosDe6Caracteres = palabras.filter(palabra => palabra.length < 6);
+console.log(menosDe6Caracteres);
+
+// Encontrar posicion de la primera palabra que acabe en 'o'
+
+let recuperarO = palabras.findIndex(palabra => palabra[palabra.length -1] ==="o");
+
+
+    // Dado un array con 8 números de teléfono, busca si alguno termina en 3.
+
+    const telefonos = [
+  "5551234567",
+  "5552345678",
+  "5553456789",
+  "5554567890",
+  "5555678901",
+  "5556789012",
+  "5557890123",
+  "5558901234"
+];
+
+console.log(telefonos);
+
 
 }
