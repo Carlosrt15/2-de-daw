0aa51efc1ee3a98412d8fa25e7b4100e88df5e01
Carlos <carlosrt123450@gmail.com>
2025-10-16 13:38:22 +0200

    Proyecto con Spring Boot

---
+++ Dev_sevidor/saludoSpringBoot/src/main/java/com/example/demo/controller/holaController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+ import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.GeneradorNumero;
+
+@Controller
+public class holaController {
+	
+	@RequestMapping("/hola")
+	
+	public String hola() {
+	return "hola";
+	}
+	
+	@RequestMapping(value="/aleatorio")
+	public String obtenerAleatorio(Model model) {
+	// 1. Crear una nueva instancia de la clase de utilidad
+	GeneradorNumero generador = new GeneradorNumero();
+	// 2. Llamar al método para obtener el número
+	int num = generador.generar();
+	// 3. Pasar el dato a la vista
+	model.addAttribute("numeroGenerado", num);
+	// 4. Devolver el nombre de la vista
+	return "numero";
+	}
+
+}
