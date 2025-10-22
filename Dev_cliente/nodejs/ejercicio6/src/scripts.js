c4e247cbadc2306a801e1bf835c1d3c4ce4a335a
Carlos <carlosrt123450@gmail.com>
2025-10-22 12:26:26 +0200

    Tomar las preferencias

--- Dev_cliente/nodejs/ejercicio6/src/scripts.js
+++ Dev_cliente/nodejs/ejercicio6/src/scripts.js
@@ -3,5 +3,20 @@ function darAlta(){
 let nombre = document.getElementById("nombre").value;
 let apellidos = document.getElementById("apellidos").value;
 let dni = document.getElementById("dni").value;
+let fecha = document.getElementById("fecha").value;
+
+// mostrar sexo
+let sexo = document.querySelector('input[name="sexo"]:checked');
+let mostraSexo = sexo.value
+
+// mostrar checkboxes
+let seleccionadas = document.querySelectorAll('input[name="pref"]:checked');
+
+let preferencias = Array.from(seleccionadas).map(input => input.value);
+
+console.log(nombre,apellidos,dni,fecha,mostraSexo,preferencias);
+
+
+
 
 }
