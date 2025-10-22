0fd2db8df4f38a62c098f3d43174ab9efc01ad88
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-10-22 07:44:37 +0200

    update ejercicio

---
+++ Dev_cliente/nodejs/ejercicio6/src/scripts.js
@@ -0,0 +1,7 @@
+function darAlta(){
+
+let nombre = document.getElementById("nombre").value;
+let apellidos = document.getElementById("apellidos").value;
+let dni = document.getElementById("dni").value;
+
+}
