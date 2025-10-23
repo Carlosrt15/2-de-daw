6e94472776e71ee2ae1ffde55dd78e7e8e63d778
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-10-23 08:44:51 +0200

    update del ejercicio 6

--- Dev_cliente/nodejs/ejercicio6/src/scripts.js
+++ Dev_cliente/nodejs/ejercicio6/src/scripts.js
@@ -16,8 +16,43 @@ let preferencias = Array.from(seleccionadas).map(input => input.value);
 
 console.log(nombre,apellidos,dni,fecha,mostraSexo,preferencias);
 
+// Crear objeto 
 
+let nuevoCliente = {
 
+    nombre: nombre,
+    apellidos: apellidos,
+    DNI: dni,
+    fechaNac: fecha,
+    Sexo: sexo,
+    preferencias: preferencias
+};
 
+//  Configurar petiucion 
+
+    let URL = "http://localhost:3000/clientes";
+    let init = {
+      method: 'POST',
+      body: JSON.stringify(nuevoCliente),
+      headers: { 'Content-Type': 'application/json' }
+    };
+
+    fetch(URL, init)
+    .then(response => response.json())
+    .then(datosRespuesta => {
+        console.log("Cliente dado de alta:"+ datosRespuesta);
+      alert("Cliente dado de alta correctamente");
+
+    })
+
+        .catch(error => console.error("Error al dar de alta:"+ error));
 }
+    
 
+/**json-server --watch clientes.json --port 3000 */
+
+
+function borrarCliente(){
+
+    
+}
