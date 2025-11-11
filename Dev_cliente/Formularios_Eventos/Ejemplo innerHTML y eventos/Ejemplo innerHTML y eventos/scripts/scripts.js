fee2fd0e4c67651f2172617afc6b7ee13a4ae50a
Carlos <carlosrt123450@gmail.com>
2025-11-11 14:06:08 +0100

    para casa los formularios

--- Dev_cliente/Formularios_Eventos/Ejemplo innerHTML y eventos/Ejemplo innerHTML y eventos/scripts/scripts.js
+++ Dev_cliente/Formularios_Eventos/Ejemplo innerHTML y eventos/Ejemplo innerHTML y eventos/scripts/scripts.js
@@ -1,30 +1,38 @@
 window.onload = function () {
-  document
-    .getElementById("tablaDatos")
-    .addEventListener(
-      "click",
-      crearNuevaFila.bind(document.getElementById("tablaDatos")),
-      false
-    );
+  const tabla = document.getElementById("tablaDatos");
+  tabla.addEventListener(
+    "click",
+    crearNuevaFila.bind(tabla),
+    false
+  );
 };
+
+
 //Ejemplo Punto 1.7 de los apuntes
 function crearNuevaFila() {
   let numFilas = this.childElementCount;
-  let fila =
-    "<tr id='" +
-    numFilas +
-    "'><td>" +
-    (numFilas + 1) +
-    "</td><td>" +
-    (numFilas + 2) +
-    "</td></tr>";
-  this.innerHTML += fila;
-  document.getElementById(numFilas).addEventListener(
-    "click",
-    (event) => {
-      alert("Id: " + event.currentTarget.id);
-      event.stopPropagation();
-    },
-    false
-  );
+
+  let fila = document.createElement("tr");
+  fila.id = numFilas;
+
+  let celda1 = document.createElement("td");
+  celda1.textContent = numFilas + 1;
+
+  let celda2 = document.createElement("td");
+  celda2.textContent = numFilas + 2;
+
+  fila.appendChild(celda1);
+  fila.appendChild(celda2);
+
+  fila.addEventListener("click", (event)=>{
+    alert("ID:" + event.currentTarget.id)
+    event.stopPropagation();
+  },
+  false
+);
+
+this.appendChild(fila);
+
 }
+
+
