3f749d2643fc2808212267acb46d448111cba200
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-11-12 17:29:59 +0100

    ejercicio 4 _ 2 punto terminado

--- Dev_cliente/Formularios_Eventos/ejercicio 4/src/codigo.js
+++ Dev_cliente/Formularios_Eventos/ejercicio 4/src/codigo.js
@@ -1,22 +1,34 @@
 function Comprobar() {
     let tomarEjemplo = document.getElementById("campoEjemplo").value;
     let tomarTlfn = document.getElementById("tlfn").value;
-    let tomarObs =  document.getElementById("observaciones").value;
+    let tomarObs = document.getElementById("observaciones").value;
     let tomarRadio = document.querySelector('input[name="prepago"]:checked');
-    let tomarCompania = document.querySelector('input[name="compania"]:checked');
+    let tomarCompania = document.getElementById("compania").value;
     // Puede estar vacio
     let tomarFranjaHoraria = document.querySelector('input[name="franja"]:checked');
 
+    //  Mostrar valor
+    let mostrar1 = tomarRadio.value;
+    let mostrar2 =  tomarFranjaHoraria.value;
+
 
     // comprobar que todos lo campos tiene un valor 
-        if(tomarCompania != "" && tomarTlfn != "" && tomarObs != "" && tomarRadio != ""&& tomarEjemplo != ""){
-            alert("los campos a excepcion de el checkbox tienen valor o fueron seleccionados ");
-        } else {
-            alert("Hay campos sin seleccionar o vacios ");
 
-            //Continuar aqui 
-            
-        }
+    let faltan = [];
+
+    if (tomarEjemplo === "") faltan.push("Ejemplo");
+    if (tomarTlfn === "") faltan.push("Teléfono");
+    if (tomarObs === "") faltan.push("Observaciones");
+    if (!tomarRadio) faltan.push("Prepago");
+    if (tomarCompania === "") faltan.push("Compañía");
+    if (!tomarFranjaHoraria) faltan.push("Horario");
+
+    if (faltan.length === 0) {
+        alert("los campos a excepciol de checbox tienen contenido");
+            alert(tomarCompania + tomarTlfn + tomarObs + mostrar1 +  tomarEjemplo + mostrar2);
+    } else {
+        alert("Algunos campos estan vacios: " +faltan.join(","));
+    }
 
 }
 
