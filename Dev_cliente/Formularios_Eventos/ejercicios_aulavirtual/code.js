3c5037e19cb8a95e24337f0b01f7458583b197e7
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-11-13 20:50:14 +0100

    perdido

--- Dev_cliente/Formularios_Eventos/ejercicios_aulavirtual/code.js
+++ Dev_cliente/Formularios_Eventos/ejercicios_aulavirtual/code.js
@@ -29,16 +29,43 @@ function ejercicio1() {
 
 }
 
-function dni(){
+function dni() {
     let toamrP = document.getElementsByTagName("p")[0];
     let tomarBody = document.getElementsByTagName("body")[0];
-    
 
 
-    tomarBody.addEventListener("keydown",(e)=> {
+
+    tomarBody.addEventListener("keydown", (e) => {
         console.log("Se pulso tecla");
         let tecla = e.key;
         alert(tecla);
+
+        let a0 = "T";
+        let a1 = "R";
+        let a2 = "W";
+        let a3 = "A";
+        let a4 = "G";
+        let a5 = "M";
+        let a6 = "Y";
+        let a7 = "F";
+        let a8 = "P";
+        let a9 = "D";
+        let a10 = "X";
+        let a11 = "B";
+        let a12 = "N";
+        let a13 = "J";
+        let a14 = "Z";
+        let a15 = "S";
+        let a16 = "Q";
+        let a17 = "V";
+        let a18 = "H";
+        let a19 = "L";
+        let a20 = "C";
+        let a21 = "K";
+        let a22 = "E";
+
+
+
 
     });
 
