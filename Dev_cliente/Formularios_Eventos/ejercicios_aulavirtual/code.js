efe5280e50ddd24b5fe3d737fd55185185b68df7
Carlos <carlosrt123450@gmail.com>
2025-11-13 10:07:02 +0100

    Terminado el ejercicio 1

---
+++ Dev_cliente/Formularios_Eventos/ejercicios_aulavirtual/code.js
@@ -0,0 +1,32 @@
+// ejercicio 1
+
+function ejercicio1() {
+
+    let aviso = document.getElementsByTagName("button")[0];
+    let pararAviso = document.getElementsByTagName("button")[1];
+
+    let intervalo;
+
+    // evento para el botón Aviso
+    aviso.addEventListener("click", (e) => {
+        // solo crear el intervalo si no hay uno activo
+        if (!intervalo) {
+            intervalo = setInterval(() => {
+                alert("Este sábado a las 3 son las 2");
+            }, 5000);
+            console.log("click en Aviso");
+        }
+    });
+
+
+    // evento para el botón Parar Aviso
+    pararAviso.addEventListener("click", (a) => {
+        clearInterval(intervalo);
+        intervalo = null; // limpia la variable
+        console.log("click en Parar Aviso");
+    });
+
+
+}
+
+ejercicio1();
