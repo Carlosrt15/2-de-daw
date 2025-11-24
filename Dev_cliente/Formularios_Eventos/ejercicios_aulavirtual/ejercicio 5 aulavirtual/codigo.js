3ca42d0ecc5f5aba63e610a8e8575884eedf906f
Carlos <carlosrt123450@gmail.com>
2025-11-24 10:15:24 +0100

    Comprobar bien que son anagrmas con dblclick

--- Dev_cliente/Formularios_Eventos/ejercicios_aulavirtual/ejercicio 5 aulavirtual/codigo.js
+++ Dev_cliente/Formularios_Eventos/ejercicios_aulavirtual/ejercicio 5 aulavirtual/codigo.js
@@ -28,5 +28,43 @@ function comprobarCorreo() {
     let tomarCorreo = document.getElementById("correo").value;
     let comprobar = /@(gmail\.com|hotmail\.com|calasanzsalamanca\.com)$/i;
 
+    if (comprobar.test(tomarCorreo)) {
+        alert("Correo correcto");
+    } else {
+        alert("Correo incorrecto");
+    }
+    console.log(tomarCorreo);
+}
+
+function comprobarTexto() {
+    let tomarTexto1 = document.getElementById("texto1").value;
+    let tomarTexto2 = document.getElementById("texto2").value;
+
+    if (tomarTexto1 !== "" && tomarTexto2 !== "") {
+
+        ordenar1 = tomarTexto1.toUpperCase().split('').sort().join('');
+        ordenar2 = tomarTexto2.toUpperCase().split('').sort().join('');
+
+        return ordenar1 === ordenar2;
+
+
+
+    } else {
+        alert("No se han introducido textos");
+
+    }
+
 
 }
+
+function comprobarAnagrama() {
+
+    let resultado = comprobarTexto();
+
+    if (resultado === true) {
+        console.log("Son anagramas");
+    } else {
+        console.log(" No son anagramas");
+    }
+}
+
