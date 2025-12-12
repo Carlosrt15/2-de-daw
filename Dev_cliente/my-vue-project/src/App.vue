1633fd37cd7d2bca7173af8460f39e9438f98d19
Carlos <carlosrt123450@gmail.com>
2025-12-12 10:31:05 +0100

    El proyecto de vue no me funciona

--- Dev_cliente/my-vue-project/src/App.vue
+++ Dev_cliente/my-vue-project/src/App.vue
@@ -1,5 +1,10 @@
-<script setup></script>
+
 <template>
   <h1>Hola a todos</h1>
+  <Formulario />
 </template>
+<script>
+  
+import Formulario from './components/Formulario.vue';
+</script>
 <style></style>
---
+++ Dev_cliente/my-vue-project/src/components/Formulario.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="enviarFormulario">
+      <div>
+          <label for="Nombre">Nombre</label>
+          <input v-model="form.nombre" type="text" id="Nombre" />
+      </div>
+
+      <div>
+        <label for="Mensaje">Mensaje:</label>
+        <textarea v-model="form.mensaje" id="Mensaje" placeholder="Aqui saldra la respuesta"></textarea>
+      </div>
+  </form>
+</template>
+
+<script src="./Formulario.js"></script>
+
+<style>
+form {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  gap: 10px;
+}
+</style>
