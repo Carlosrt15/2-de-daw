25b5c3fa4331eab5b5aab3b090f13d194b9b09b7
Carlos <carlosrt123450@gmail.com>
2025-12-15 10:25:20 +0100

    Ya se muestran los artista y ahora hay que mostrar cada info en la tabla

--- Dev_cliente/PracticaCompleta1/scripts/Codigo.js
+++ Dev_cliente/PracticaCompleta1/scripts/Codigo.js
@@ -7,6 +7,16 @@ function verInfo() {
     crearH2.textContent = 'Información de los artistas';
     recuperarZonaDatos.appendChild(crearH2);
 
+  
+    // Funcion parara recuperar los artista y Crear desplegable 
+    let select2 = document.createElement("select");
+    select2.id = "selectArtistas";
+    recuperarZonaDatos.appendChild(select2);
+
+    cargarArtistasEnSelect();
+
+
+
     // Crear tabla ---------------------------
     let tabla = document.createElement("table");
     let thead = document.createElement("thead");
@@ -32,9 +42,31 @@ function verInfo() {
 
     recuperarZonaDatos.appendChild(tabla);
 
-
-
-
+    
 
 }
 
+function cargarArtistasEnSelect() {
+    fetch("http://localhost:3000/artistas")
+        .then(respuesta => respuesta.json())
+        .then(artistas => {
+
+
+            artistas.sort((a, b) => a.nombre.localeCompare(b.nombre));
+
+            let select = document.getElementById("selectArtistas");
+
+            let opcionDefault = document.createElement("option");
+            opcionDefault.textContent = "Selecciona un artista";
+            opcionDefault.value = "";
+            select.appendChild(opcionDefault);
+
+            artistas.forEach(artistas => {
+                let option =document.createElement("option");
+                option.value = artistas.id;
+                option.textContent = artistas.nombre;
+                select.appendChild(option);
+            });
+            
+        });
+}
