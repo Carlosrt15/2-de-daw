06ce804ad960b9478ca20c5fd47140b565549769
Carlos <carlosrt123450@gmail.com>
2026-01-14 14:08:45 +0100

    datos rellenados del cliente

--- Dev_cliente/Vue.js/ejercicios-vue-adicionales/scripts/js.js
+++ Dev_cliente/Vue.js/ejercicios-vue-adicionales/scripts/js.js
@@ -1,1 +1,34 @@
 const { createApp, ref, reactive } = Vue;
+
+const app1 = createApp({
+    setup() {
+        const datos = ref([
+            {
+                nombre: "Diego", apellidos: "González Serrano", direccion: "Alfonso X 17 5ºD",
+                telefono: 654123987, estadoCivil: "casado",
+                productos: [{
+                    id: 1,
+                    nombre: 'Cuenta Nómina',
+                    saldo: 3597
+                },
+                {
+                    id: 2,
+                    nombre: 'Plan de Pensiones',
+                    saldo: 25432
+                },
+                {
+                    id: 3,
+                    nombre: 'Fondo de Inversión',
+                    saldo: 65198
+                }
+                ]
+
+            },
+            {}
+        ]);
+
+        return {
+            datos
+        };
+    }
+});
