71cfe600c68cac8d7f2d8229fe339d63644bfefd
Carlos <carlosrt123450@gmail.com>
2026-01-21 14:00:35 +0100

    termianr el resto en casa falta contadores y cambiar el nombre

--- Dev_cliente/Vue.js/EJERCICIO3-+-IMAGENES/scripts/main.js
+++ Dev_cliente/Vue.js/EJERCICIO3-+-IMAGENES/scripts/main.js
@@ -1,39 +1,48 @@
-const {createApp, ref, reactive} = Vue;
+const { createApp, ref, reactive } = Vue;
 
 const app = createApp({
     setup() {
-        let estilosBorde = ref([true,true,true]);
+
+
+        let estilosBorde = ref([true, true, true]);
+        let nombreTemporal = ref(["","",""]);
 
         let datosImagenes = ref([
             {
-            url:"./img/Colise1.jpg",
-            nombre:"Colise1",
-            contador: 0
+                url: "./img/Colise1.jpg",
+                nombre: "Colise1",
+                contador: 0
             },
             {
-            url:"./img/Colise1.jpg",
-            nombre:"Colise2",
-            contador: 0
+                url: "./img/Colise1.jpg",
+                nombre: "Colise2",
+                contador: 0
             },
+
             {
-            url:"./img/Grecia3.jpg",
-            nombre:"Postureo Grecia",
-            contador: 0
+                url: "./img/Grecia3.jpg",
+                nombre: "Postureo Grecia",
+                contador: 0
             }
         ]);
 
         let cambiarEstiloBorde = (index) => {
             estilosBorde.value[index] = !estilosBorde.value[index];
-        }
+        };
 
         let aumentarContador = (index) => {
             datosImagenes.value[index].contador++;
+        };
+        
+        let cambiarNombre = (index) => {
+            
         };
         return {
             datosImagenes,
             estilosBorde,
             cambiarEstiloBorde,
-            aumentarContador
+            aumentarContador,
+            nombreTemporal
         }
     }
 }).mount("#app");
