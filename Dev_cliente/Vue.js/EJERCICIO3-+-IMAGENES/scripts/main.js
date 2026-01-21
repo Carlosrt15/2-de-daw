d632f87991d7f6ff2dd6847c17636ace06155494
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2026-01-21 18:51:02 +0100

    Contador funcionando

--- Dev_cliente/Vue.js/EJERCICIO3-+-IMAGENES/scripts/main.js
+++ Dev_cliente/Vue.js/EJERCICIO3-+-IMAGENES/scripts/main.js
@@ -5,7 +5,7 @@ const app = createApp({
 
 
         let estilosBorde = ref([true, true, true]);
-        let nombreTemporal = ref(["","",""]);
+        let nombreTemporal = ref(["", "", ""]);
 
         let datosImagenes = ref([
             {
@@ -33,16 +33,19 @@ const app = createApp({
         let aumentarContador = (index) => {
             datosImagenes.value[index].contador++;
         };
-        
+
         let cambiarNombre = (index) => {
-            
+           datosImagenes.value[index].nombre = nombreTemporal.value[index];
+           nombreTemporal.value[index] = "";
+
         };
         return {
             datosImagenes,
             estilosBorde,
             cambiarEstiloBorde,
             aumentarContador,
-            nombreTemporal
+            nombreTemporal,
+            cambiarNombre
         }
     }
 }).mount("#app");
