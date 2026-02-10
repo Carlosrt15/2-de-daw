60ae7f2c7a89d3392b346f23bc099261d4785267
Carlos <carlosrt123450@gmail.com>
2026-02-10 12:41:39 +0100

    se muestran los grupos incompletos

--- Dev_cliente/EJERCICIOS-ADICIONALES-VUE/PersonasBici/personasBici/src/App.vue
+++ Dev_cliente/EJERCICIOS-ADICIONALES-VUE/PersonasBici/personasBici/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import FormularioPreferencias from './components/formularioPreferencias.vue';
+import TablaGruposEnFormacion from './components/tablaGruposEnFormacion.vue';
 
 
 
@@ -9,14 +10,50 @@ let altaUsuarios = ref([
 ]);
 
 const agregarUsuario = (nuevoUsuario) => {
-  altaUsuarios.value.push(nuevoUsuario);
+  // Buscar grupo existente
+  let grupo = gruposEnFormacion.value.find(
+    g =>
+      g.dia === nuevoUsuario.diaSemana &&
+      g.franja === nuevoUsuario.disponibilidad
+  );
+
+  if (!grupo) {
+    // Crear nuevo grupo
+    grupo = {
+      id: siguienteId++,
+      dia: nuevoUsuario.diaSemana,
+      franja: nuevoUsuario.disponibilidad,
+      participantes: []
+    };
+    gruposEnFormacion.value.push(grupo);
+  }
+
+  // Añadir usuario
+  grupo.participantes.push(nuevoUsuario.nombre);
+
+  // Si llega a 5 → mover a grupos formados
+  if (grupo.participantes.length === 5) {
+    gruposFormados.value.push(grupo);
+    gruposEnFormacion.value = gruposEnFormacion.value.filter(g => g !== grupo);
+  }
 };
+
+
+// Grupos
+
+const gruposEnFormacion = ref([]);
+const gruposFormados = ref([]);
+let siguienteId = 1;
+
+
+
 
 </script>
 
 <template>
   <h1>Lista de personas en bici</h1>
   <FormularioPreferencias :onSubmit="agregarUsuario"></FormularioPreferencias>
+  <TablaGruposEnFormacion :grupos="gruposEnFormacion"></TablaGruposEnFormacion>
 </template>
 
 <style scoped></style>
---
+++ Dev_cliente/EJERCICIOS-ADICIONALES-VUE/PersonasBici/personasBici/src/components/tablaGruposEnFormacion.vue
@@ -0,0 +1,60 @@
+<script setup>
+const props = defineProps(['grupos']);
+
+
+</script>
+
+
+<template>
+<section class="seccion">
+    <h2>Grupos en formacion</h2>
+
+    <table v-if="props.grupos.length > 0">
+        <thead>
+            <tr>
+                <th>Grupo</th>
+                <th>Día</th>
+                <th>Franja</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="grupo in props.grupos" :key="grupo.id">
+                <td>{{ grupo.id }}</td>
+                <td>{{ grupo.dia }}</td>
+                <td>{{ grupo.franja }}</td>
+
+            </tr>
+        </tbody>
+
+    </table>
+    <p v-else>No hay grupos en formacion</p>
+</section>
+
+</template>
+
+
+<style scoped>
+.seccion {
+  margin-top: 30px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  background-color: #f5f5f5;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 10px;
+}
+
+th, td {
+  border: 1px solid #aaa;
+  padding: 8px;
+  text-align: center;
+}
+
+th {
+  background-color: #d0eaff;
+}
+
+</style>
