e4a40a2fc243baf214cf25db6435fe8285ee373f
Carlos <carlosrt123450@gmail.com>
2026-02-18 13:55:57 +0100

    array para formatear el ejercicio debe ser una array de lista

--- Dev_cliente/VUE-22-VUE/VUE-router/src/App.vue
+++ Dev_cliente/VUE-22-VUE/VUE-router/src/App.vue
@@ -1,6 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 
+const listaAutores = ref([{
+  pais: "España", autores: ["Miguel de Cervantes", "Federico García Lorca", "Arturo Pérez-Reverte", "Javier Castillo", "Carlos Ruiz Zafón"]
+}, {
+  pais: "Argentina", autores: ["Jorge Luis Borges", "Julio Cortázar", "Ernesto Sabato", "Adolfo Bioy Casares", "Silvina Ocampo"]
+}, {
+  pais: "México", autores: ["Octavio Paz", "Juan Rulfo", "Carlos Fuentes", "Laura Esquivel", "Elena Poniatowska"]
+}, {
+  pais: "Chile", autores: ["Pablo Neruda", "Isabel Allende", "Roberto Bolaño", "Gabriela Mistral", "Nicanor Parra"]
+}, {
+  pais: "Colombia", autores: ["Gabriel García Márquez", "Álvaro Mutis", "Laura Restrepo", "Fernando Vallejo", "Jorge Isaacs"]
+}, {
+  pais: "Perú", autores: ["Mario Vargas Llosa", "César Vallejo", "Alfredo Bryce Echenique", "Clorinda Matto de Turner", "Ricardo Palma"]
+}
+])
+
+
 </script>
 
 <template>
