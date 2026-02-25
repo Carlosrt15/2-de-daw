9c0d2e530c7b281bcd3570239f3f470245112895
Carlos <carlosrt123450@gmail.com>
2026-02-25 14:08:52 +0100

    docker file update

--- docker/dockerRunInteraccion/src/HolaMundo.java
+++ docker/dockerRunInteraccion/src/HolaMundo.java
@@ -1,18 +1,29 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
 public class HolaMundo {
     public static void main(String[] args) {
         int numero = 0;
-        List lista = new ArrayList();
         Scanner scn = new Scanner(System.in);
+
         System.out.println("Introduce números. El cero terminará la ejecución");
-        do {
-            if(scn.hasNextInt()) {			
-                numero = scn.nextInt(); 
-                lista.add(numero);
-            }
-        } while (numero!=0);
+
+        try {
+            FileWriter fw = new FileWriter("/app/BBDD/letras.txt", true);
+
+            do {
+                if(scn.hasNextInt()) {			
+                    numero = scn.nextInt(); 
+                    fw.write(numero + "\n");
+                }
+            } while (numero != 0);
+
+            fw.close();
+
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo");
+            e.printStackTrace();
+        }
     }
 }
