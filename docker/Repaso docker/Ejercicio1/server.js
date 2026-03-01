ff5bcbddad64809b8bb7f24fe55d5328ae2827a2
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2026-03-01 18:05:39 +0100

    Repaso examen de docker

---
+++ docker/Repaso docker/Ejercicio1/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+// Usamos variable de entorno o 3000 por defecto
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Hola Mundo desde Docker!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en puerto ${PORT}`);
+});
