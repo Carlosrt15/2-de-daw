85fa4ca90ddf06cfaa64acb75b2b558cf78aa275
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2026-03-03 21:26:51 +0100

    cambios para el exmane de mañana

---
+++ Dev_cliente/NODE-NASA-AULAVIRTUAL/NasaVue/src - copia/components/MenuHorizontal.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+<h2>Menu Horizontal</h2>
+<nav>
+    <ul>
+        <li><RouterLink to="/">Panel Principal</RouterLink></li>
+        <li><RouterLink to="/imagen">Imagen del dia</RouterLink></li>
+        <li><RouterLink to="/astCercano">Asteroides Cercanos</RouterLink></li>
+        <li> <RouterLink to="busquedaID">Busqueda por ID</RouterLink></li>
+        <li> <RouterLink to="acercaDe">Acerca del centro</RouterLink> </li>
+    </ul>
+</nav>
+<RouterView></RouterView>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ docker/Repaso-DOKER/Ejercicio aula virtual completo/Calendario/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="contenedor">
+    <Calendario
+      :citas="citas"
+      :diaSeleccionado="diaSeleccionado"
+      @seleccionar-dia="diaSeleccionado = $event"
+      @guardar-cita="guardarCita"
+    />
+
+    <CajaCitas
+      :dia="diaSeleccionado"
+      :cita="citas[diaSeleccionado]"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import Calendario from "./components/Calendario.vue"
+import CajaCitas from "./components/CajaCitas.vue"
+
+const citas = ref({})
+const diaSeleccionado = ref(null)
+
+function guardarCita({ dia, texto }) {
+  citas.value[dia] = texto
+}
+</script>
+
+
+<style>
+.contenedor {
+  display: flex;
+  padding: 20px;
+  gap: 20px;
+}
+</style>
